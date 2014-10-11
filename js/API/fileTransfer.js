956e9c456ff83f8abca0877cdca3d2f8f97c95c4
Amram López Ochoa <amram_x@hotmail.com>
2014-10-11 12:47:28 -0500

    15
    15

--- js/API/fileTransfer.js
+++ js/API/fileTransfer.js
@@ -1,24 +1,24 @@
-// fileTransfer.js
+//fileTransfer.js
 var myTransfer = {
-	subido: function(r){
-		if(r.response == '1'){
-			navigator.notification.alert("Se ha registrado correctamente",function(){
-				window.location.href = '#home';
-				//Asignar Registro local
-			},"Felicidades","Aceptar");
-		}
-	},
-	error: function(err){
-		navigator.notification.alert("Error: "+err.code,null,"Error","Aceptar");
-},
-
-subir: function(uri,url){
-	myTransfer.opciones.fileKey="foto";
-	myTransfer.opciones="Amram";
-	myTransfer.opciones.mimeType="image/jpeg";
-	myTransfer.opciones.params = {value1: 'Text', value2: 'param'};
-
-	myTransfer.ft = new FileTransfer();
-	myTransfer.ft.upload(uri,url,myTransfer.subido,myTransfer.error,myTransfer.opciones);
-	}
+    subido: function(r){
+        if(r.response == '1'){
+            navigator.notification.alert("Se ha registrado correctamente",function(){
+                window.location.href = '#home';
+                //Asignar Registro Local
+            },"Felicidades","Aceptar");
+        }
+    },
+    error: function(err){
+        navigator.notification.alert("Error: "+err.code,null,"Error","Aceptar");
+    },
+    subir: function(uri,url){
+        myTransfer.opciones = new FileUploadOptions();
+        myTransfer.opciones.fileKey = "foto";
+        myTransfer.opciones.fileName = "Amram";
+        myTransfer.opciones.mimeType = "image/jpeg";
+        myTransfer.opciones.params = {value1: 'Text',value2: 'param'};
+        
+        myTransfer.ft = new FileTransfer();
+        myTransfer.ft.upload(uri,url,myTransfer.subido,myTransfer.error,myTransfer.opciones);
+    }
 };
--- js/actions.js
+++ js/actions.js
@@ -1,37 +1,39 @@
-// action.js
+//actions.js
 var fn = {
-	init: function(){
-		document.addEventListener('deviceready',fn.device,false);
-	},
-	device: function(){
-		var x = false;
-		if(!x)
-			window.location.href = '#reg';
-		$('#regTake').tap(myCapture.tomarFoto);	
-		$('#regSend').tap(fn.registro);
-	},
-	registro: function(){
-		var nombre = $('#regName').val();
-		var mail = $('#regMail').val();
-		var tel = $('#regTel').val();
-		var foto = $('#regTake').attr('rel');
-		if(nombre != '' && mail != '' && tel != '' && foto != '' && foto != udefined){
-			//Enviar datos al servidor
-			fn.enviarRegistro(nombre, mail, tel, foto);
-		}else{
-			navigator.notification.alert("Todos los campos son requeridos.",null,'Error','Aceptar');
-		}
-	},
-	enviarRegistro: function(nom, mail, tel, foto){
-		$.ajax({
-  			type: "POST",
-  			url: "http://carlos.igitsoft.com/apps/test.php",
-  			data: {nom:nom,mail:mail,tel:tel}
-		}).done(function(respuesta) {
-    		if(respuesta == 1){
-				myTransfer.subir(foto,"http://carlos.igitsoft.com/apps/test.php");
-			}
-  		});
-	}
+    init: function(){
+        //document.addEventListener("deviceready", yourCallbackFunction, false); //phonga
+        document.addEventListener('deviceready',fn.device,false);
+        //$('#regSend').click(); //Producir un click
+    },
+    device: function(){
+        var x = false;
+        if(!x)
+            window.location.href = '#reg';
+        $('#regTake').tap(myCapture.tomarFoto);
+        $('#regSend').tap(fn.registro);
+    },
+    registro: function(){
+        var nombre = $('#regName').val();
+        var mail = $('#regMail').val();
+        var tel = $('#regTel').val();
+        var foto = $('#regTake').attr('rel');
+        if(nombre != '' && mail != '' && tel != '' && foto != '' && foto != undefined){
+            //Enviar datos al Servidor.
+            fn.enviarRegistro(nombre, mail, tel, foto);
+        }else{
+            navigator.notification.alert("Todos los campos son requeridos.",null,'Error','Aceptar');
+        } 
+    },
+    enviarRegistro: function(nom, mail, tel, foto){
+        $.ajax({
+            type: "POST",
+            url: "http://carlos.igitsoft.com/apps/test.php",
+            data: {nom:nom,mail:mail,tel:tel}
+        }).done(function(respuesta){
+            if( respuesta == '1' ){
+                myTransfer.subir(foto, "http://carlos.igitsoft.com/apps/test.php");
+            }
+        });
+    }
 };
 $(fn.init);
