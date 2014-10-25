19cf3f13d1815cfc9b72f95e4454db63a5f59d29
Amram López Ochoa <amram_x@hotmail.com>
2014-10-25 11:29:09 -0500

    39
    39

--- js/API/websql.js
+++ js/API/websql.js
@@ -19,33 +19,30 @@ var db = {
         $.mobile.loading( 'hide' );
         navigator.notification.alert('Reserva en espera de conexión',null,'Guardado','Aceptar');
     },
-	
-	leerPendientes: function(){
-		db.crearDB().transaction(db.selectPendientes,db.error,null);
-	},
-	aelectPendientes: function(tx){
-		tx.executeSql("SELECT * FROM pendientes",[],db.resultadosPendientes,db.error);
-	},
-	resultadosPendientes: function(tx,res){
-		var cant = res.rows.length;
-		if(cant>0){
-			var th = '';
-			var ha = '';
-			var pr = '';
-			var di = '';
-			
-			for(var i=0;i<cant;i++){
-				var th = res.rows.item(i).th;
-				var ha = res.rows.item(i).ha;
-				var pr = res.rows.item(i).pr;
-				var di = res.rows.item(i).di;
-				
-				fn.enviarReserva(th,ha,tr,di);
-				tx.executeSql("DELETE FROM pendientes WHERE id = '" + res.rows.item(i).id + "'");
-			}
-		}
-	},
-	
+    leerPendientes: function(){
+        alert(1);
+        db.crearDB().transaction(db.selectPendientes,db.error,null);
+    },
+    selectPendientes: function(tx){
+        alert(2);
+        tx.executeSql("SELECT * FROM pendientes",[],db.resultadosPendientes,db.error);
+    },
+    resultadosPendientes: function(tx,res){
+        alert(3);
+        var cant = res.rows.length;
+        if(cant>0){
+            for(var i = 0;i < cant;i++){
+                alert(i);
+                var th = res.rows.item(i).th;
+                var ha = res.rows.item(i).ha;
+                var pr = res.rows.item(i).pr;
+                var di = res.rows.item(i).di;
+                
+                fn.enviarReserva(th,ha,pr,di);
+                tx.executeSql("DELETE FROM pendientes WHERE id='" + res.rows.item(i).id + "'");
+            }
+        }
+    },
     //-------------------HISTORIAL---------------------
     agregarHistorial: function(th,ha,pr,di){
         db.th = th;
